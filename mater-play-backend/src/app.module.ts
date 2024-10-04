6df9338313ecd189ed5f903b7ac67a356f821422
DanielBrisch <danielbrisch157@gmail.com>
2024-10-03 21:59:21 -0300

    feat: adicionando back end

---
+++ mater-play-backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule { }
---
+++ mater-play-backend/src/entities/movie-entity.ts
@@ -0,0 +1,7 @@
+export class Movie {
+    id: string;
+    title: string;
+    descripition: string;
+    ageRatting: string;
+    poster: string;
+}
